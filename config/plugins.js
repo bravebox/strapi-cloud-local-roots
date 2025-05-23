c01fc4b78a9c8b1dd631c839c17594c20e7e07cf
Mick Schouten <mick.schouten@officebooking.com>
2025-05-23 14:48:16 +0200

    Added email plugin and settings

--- config/plugins.js
+++ config/plugins.js
@@ -1,6 +1,28 @@
+const { env } = require('@strapi/utils');
+
 module.exports = () => ({
   // 'month-multi-selector': {
   //   enabled: true,
   //   resolve: './src/plugins/month-multi-selector'
   // },
+
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false, // true for port 465, false for port 587
+        auth: {
+          user: env('SMTP_USERNAME'), // your Gmail address
+          pass: env('SMTP_PASSWORD'), // your Gmail app password
+        },
+      },
+      settings: {
+        defaultFrom: env('SMTP_USERNAME'), // your Gmail address
+        defaultReplyTo: env('SMTP_USERNAME'),
+      },
+    },
+  },
+
 });
