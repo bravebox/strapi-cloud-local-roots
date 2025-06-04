2ffa29a864e77c00b8373fb0f5538b569fbf65fb
Mick Schouten <mick@Micks-Mac-mini.local>
2025-06-04 10:29:50 +0200

    Update for graphql plugin

--- config/plugins.js
+++ config/plugins.js
@@ -4,7 +4,15 @@ module.exports = () => ({
 
   graphql: {
     config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
       playgroundAlways: true,
+      introspection: true,
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
     },
   },
 
